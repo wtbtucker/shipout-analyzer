c1e55ab945d2d6afcc3339e6c7dd7f8159fe18ff
wtucker <wtbtucker@gmail.com>
2026-06-10 12:31:37 -0400

    Set up basic streamlit app which connects to s3 files

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from st_files_connection import FilesConnection
+import boto3
+
 
 conn = st.connection("s3", type=FilesConnection)
-upc_file = conn.read("rics-file-exports/upc_export.csv", input_format="csv", ttl=600)
-print("UPC export length", len(upc_file))
+upc_df = conn.read("rics-file-exports/upc_export.csv", input_format="csv", ttl=600)
+st.write(f"UPC export length {len(upc_df)}")
